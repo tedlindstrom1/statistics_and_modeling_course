2d1e15e5448e3f0dd0e51a1c34cc2b9a1f9a9a2a
Ted Lindström <ted.lindstrom@hotmail.com>
2023-11-06 13:32:21 +0100

    Updated log_and_cv solution

--- log_and_cv.R
+++ log_and_cv.R
@@ -1,10 +1,13 @@
-#set.seed(1)
+set.seed(1)
 cv_values <- NULL
 log_values <- NULL
-for (i in 1:1000){
-  x = rnorm(n=100, mean=5, sd=1)
+sdvals <- runif(200,2,5)
+for (i in 1:100){
+  x = rnorm(n=100, mean=5, sd=3)
   cv_values[i] <- sd(x)/mean(x)
   log_values[i] <- sd(log(x))
 }
+cat('cv_values: ', cv_values)
+cat('\nlog_values: ', log_values)
 plot(x=cv_values, y=log_values)
 abline(lm(log_values ~ cv_values), col='red')
